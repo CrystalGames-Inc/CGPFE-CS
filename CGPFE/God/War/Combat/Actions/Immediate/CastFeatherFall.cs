a02ee586bd618849dfef4fca57c3e6f197f7626b
CrystalGames2000 <ofirabe09@gmail.com>
2025-03-12 21:25:18 +0200

    Added 2 more actions

---
+++ CGPFE/God/War/Combat/Actions/Immediate/CastFeatherFall.cs
@@ -0,0 +1,7 @@
+﻿using CGPFE.God.War.Combat.Action.ActionTypes;
+
+namespace CGPFE.God.War.Combat.Actions.Immediate;
+
+public class CastFeatherFall() : ImmediateAction("Cast Feather Fall", false) {
+	
+}
---
+++ CGPFE/God/War/Combat/Actions/Swift/CastQuickenedSpell.cs
@@ -0,0 +1,7 @@
+﻿using CGPFE.God.War.Combat.Action.ActionTypes;
+
+namespace CGPFE.God.War.Combat.Actions.Swift;
+
+public class CastQuickenedSpell() : SwiftAction("Cast a quickened spell", false) {
+	
+}
