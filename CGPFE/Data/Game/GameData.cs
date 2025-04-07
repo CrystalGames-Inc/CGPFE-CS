c777921c869e2c132c75501122e79dcaa23f9878
CrystalGames2000 <ofirabe09@gmail.com>
2025-04-07 23:25:48 +0300

    Fixed game data creation

--- CGPFE/Data/Game/GameData.cs
+++ CGPFE/Data/Game/GameData.cs
@@ -4,102 +4,60 @@ using CGPFE.World;
 
 namespace CGPFE.Data.Game;
 
-public class GameData {
+public class GameData(string campaignName, int gameFantasty, int gameSpeed, int abilityScoreType) {
 
-	public string CampaignName { get; set; }
+	public string CampaignName { get; set; } = campaignName;
 
-	public int GameFantasty { get; set; }
-	
-	public int GameSpeed { get; set; }
-	
-	public int AbilityScoreType { get; set; }
-	
+	public int GameFantasty { get; set; } = gameFantasty;
+
+	public int GameSpeed { get; set; } = gameSpeed;
+
+	public int AbilityScoreType { get; set; } = abilityScoreType;
+
 	public GameWorld? GameWorld;
-	
-	private readonly string _gameDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "CGPFE");
-	private readonly string _filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "CGPFE");
 
-	public GameData() {
-
-		Console.WriteLine("Create or load game?");
-		string ans = Console.ReadLine().ToUpper();
-
-		if (string.Equals(ans, "CREATE")) {
-			Console.WriteLine("Please choose a name for the campaign: ");
-			CampaignName = Console.ReadLine() ?? throw new InvalidOperationException();
-			_gameDataPath = Path.Combine(_gameDataPath, CampaignName);
-			_filePath = Path.Combine(_gameDataPath, "GameData.json");
-
-			Console.WriteLine("Please choose a game fantasty:\nLow - 1\nStandard - 2\nHigh - 3\nEpic - 4");
-			GameFantasty = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException()) - 1;
-
-			Console.WriteLine("Please choose the game speed:\nSlow - 1\nMedium - 2\nFast - 3");
-			GameSpeed = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-
-			Console.WriteLine("Please choose the method of initial point distribution:\nStandard - 1\nClassic - 2\nHeroic - 3\nPurchase - 4");
-			AbilityScoreType = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-
-			if (!Directory.Exists(_gameDataPath)) 
-				Directory.CreateDirectory(_gameDataPath);
-
-			if(!File.Exists(_filePath))
-				File.Create(_filePath).Close();
-		
-			var o = new JsonSerializerOptions() {
-				WriteIndented = true
-			};
-		
-			GameData g = new GameData(CampaignName, GameFantasty, GameSpeed, AbilityScoreType);
-		
-			var jsonString = JsonSerializer.Serialize<GameData>(g, o);
-			Console.WriteLine(jsonString);
-
-			Console.WriteLine("Writing data to file...");
-			Thread.Sleep(2000);
-		
-			try {
-				File.WriteAllText(_filePath, jsonString);
-			}
-			catch (Exception e) {
-				Console.WriteLine("Could not write data");
-				Console.WriteLine(e);
-				throw;
-			}
-
-			Console.WriteLine($"Successfully written game data to {_filePath}");
-		} else if (string.Equals(ans, "LOAD")) {
-			var campaigns = Directory.GetDirectories(_gameDataPath);
-
-			for (int i = 0; i < campaigns.Length; i++) {
-				string name = campaigns[i].Split("\\").Last();
-				campaigns[i] = name;
-			}
-			
-			Console.WriteLine("Choose campaign to load: ");
-			foreach (var directory in campaigns) {
-				Console.WriteLine(directory);
-			}
-			string lCampaign = Console.ReadLine() ?? throw new InvalidOperationException();
-			foreach (var campaign in campaigns) {
-				if (string.Equals(lCampaign, campaign)) {
-					_filePath = Path.Combine(_gameDataPath, campaign, "GameData.json");
-					GameData? loadedGameData = JsonSerializer.Deserialize<GameData>(File.ReadAllText(_filePath));
-					CampaignName = loadedGameData.CampaignName;
-					GameFantasty = loadedGameData.GameFantasty;
-					GameSpeed = loadedGameData.GameSpeed;
-					AbilityScoreType = loadedGameData.AbilityScoreType;
-					Console.WriteLine("Game successfully loaded");
-				}
-			}
+	public void DisplayGameData() {
+		Console.WriteLine($"Campaign Name: {CampaignName}");
+		switch (GameFantasty) {
+			case 0:
+				Console.WriteLine($"Game Fantasty: {Fantasty.Low}");
+				break;
+			case 1:
+				Console.WriteLine($"Game Fantasty: {Fantasty.Standard}");
+				break;
+			case 2:
+				Console.WriteLine($"Game Fantasty: {Fantasty.High}");
+				break;
+			case 3:
+				Console.WriteLine($"Game Fantasty: {Fantasty.Epic}");
+				break;
 		}
-		
 
-	}
+		switch (GameSpeed) {
+			case 0:
+				Console.WriteLine($"Game Speed: {StoryModifiers.GameSpeed.Slow}");
+				break;
+			case 1:
+				Console.WriteLine($"Game Speed: {StoryModifiers.GameSpeed.Medium}");
+				break;
+			case 2:
+				Console.WriteLine($"Game Speed: {StoryModifiers.GameSpeed.Fast}");
+				break;
+		}
 
-	public GameData(string campaignName, int gameFantasty, int gameSpeed, int abilityScoreType) {
-		CampaignName = campaignName;
-		GameFantasty = gameFantasty;
-		GameSpeed = gameSpeed;
-		AbilityScoreType = abilityScoreType;
+		switch (AbilityScoreType) {
+			case 0:
+				Console.WriteLine($"Initial Point Distribution: {StoryModifiers.AbilityScoreType.Standard}");
+				break;
+			case 1:
+				Console.WriteLine($"Initial Point Distribution: {StoryModifiers.AbilityScoreType.Classic}");
+				break;
+			case 2:
+				Console.WriteLine($"Initial Point Distribution: {StoryModifiers.AbilityScoreType.Heroic}");
+				break;
+			case 3:
+				Console.WriteLine($"Initial Point Distribution: {StoryModifiers.AbilityScoreType.Purchase}");
+				break;
+		}
 	}
 }
---
+++ CGPFE/Management/FileManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Text.Json;
+using CGPFE.Data.Game;
+
+namespace CGPFE.Management;
+
+public static class FileManager {
+	
+	private static readonly JsonSerializerOptions options = new JsonSerializerOptions() {
+		WriteIndented = true
+	};
+	
+	private static readonly string EnginePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "CGPFE");
+	private static readonly string GameDataFileName = "Settings.json";
+	private static string CampaignPath;
+
+	#region Game Data Management
+	
+	public static void NewGameData() {
+		Console.WriteLine("Please choose the campaign's name: ");
+		string campaignName = Console.ReadLine();
+
+		Console.WriteLine("Please choose a game fantasty:\nLow - 1\nStandard - 2\nHigh - 3\nEpic - 4");
+		int gameFantasty = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException()) - 1;
+		
+		Console.WriteLine("Please choose the game speed:\nSlow - 1\nMedium - 2\nFast - 3");
+		int gameSpeed = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+		Console.WriteLine("Please choose the method of initial point distribution:\nStandard - 1\nClassic - 2\nHeroic - 3\nPurchase - 4");
+		int abilityScoreType = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+		
+		GameData g = new GameData(campaignName, gameFantasty, gameSpeed, abilityScoreType);
+		CampaignPath = Path.Combine(EnginePath, campaignName);
+		if(!Directory.Exists(CampaignPath))
+			Directory.CreateDirectory(CampaignPath);
+		string gameDataPath = Path.Combine(CampaignPath, GameDataFileName);
+		CreateJsonFile(g, gameDataPath);
+	}
+
+	public static GameData LoadGameData() {
+		string[] campaigns = Directory.GetDirectories(EnginePath);
+
+		for (var i = 0; i < campaigns.Length; i++) 
+			campaigns[i] = campaigns[i].Split("\\").Last();
+
+		Console.WriteLine("Choose campaign to load: ");
+		for(var i = 0; i < campaigns.Length; i++)
+			Console.WriteLine($"{i + 1}. {campaigns[i]}");
+		
+		var loadedCampaign = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+		if (loadedCampaign < 1 || loadedCampaign > campaigns.Length)
+			throw new IndexOutOfRangeException();
+		
+		var filePath = Path.Combine(EnginePath, campaigns[loadedCampaign - 1], GameDataFileName);
+		var g = JsonSerializer.Deserialize<GameData>(File.ReadAllText(filePath), options)!;
+		
+		return g;
+	}
+	
+	#endregion
+
+	#region General Data Management
+	
+	public static void CreateJsonFile(object obj, string path) {
+		while (true) {
+			string jsonString = JsonSerializer.Serialize(obj, options);
+
+			if (File.Exists(path)) {
+				Console.WriteLine($"File already exists at {path}\nOverwrite? [Y/N]");
+				var ans = Console.ReadLine() ?? throw new InvalidOperationException();
+				if (string.Equals(ans, "N", StringComparison.OrdinalIgnoreCase))
+					return;
+				if (string.Equals(ans, "Y", StringComparison.OrdinalIgnoreCase)) {
+					try {
+						File.Create(path).Close();
+						File.WriteAllText(path, jsonString);
+
+						Console.WriteLine($"File successfully created at {path}");
+					}
+					catch (Exception e) {
+						Console.WriteLine(e);
+					}
+				}
+				else {
+					Console.WriteLine("Invalid input");
+					continue;
+				}
+			}
+			else {
+				File.Create(path).Close();
+				File.WriteAllText(path, jsonString);
+
+				Console.WriteLine($"File successfully created at {path}");
+			}
+
+			break;
+		}
+	}
+	
+	public static object ReadJsonFile(string path) {
+		object obj;
+		try {
+			obj = JsonSerializer.Deserialize<dynamic>(File.ReadAllText(path)) ?? throw new InvalidOperationException();
+
+			Console.WriteLine("Successfully read json file");
+		}
+		catch (Exception e) {
+			Console.WriteLine(e);
+			throw;
+		}
+
+		return obj;
+	}
+	
+	#endregion
+}
