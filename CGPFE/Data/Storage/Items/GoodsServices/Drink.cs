a435747e343a159daae954fe203628928a799cfa
CrystalGames2000 <ofirabe09@gmail.com>
2025-04-13 20:21:37 +0300

    Added all beverages

--- CGPFE/Data/Storage/Items/GoodsServices/Drink.cs
+++ CGPFE/Data/Storage/Items/GoodsServices/Drink.cs
@@ -3,6 +3,8 @@
 namespace CGPFE.Data.Storage.Items.GoodsServices;
 
 public static class Drinks {
+	#region Alcoholic Beverages
+	
 	public static Drink AbsintheGlass = new Drink("Absinthe (Glass)", 0, 3, true);
 	public static Drink AbsintheBottle = new Drink("Absinthe (Bottle)", 1, 30, 1, true);
 	public static Drink AleGallon = new Drink("Ale (Gallon)", 2, 0.2, true);
@@ -18,6 +20,35 @@ public static class Drinks {
 	public static Drink Godsbrew = new Drink("Godsbrew", 12, 0.05, 0.5, true);
 	public static Drink Grog = new Drink("Grog", 13, 0.02, 0.5, true);
 	public static Drink Kumis = new Drink("Kumis", 14, 0.5, 1, true);
-	public static Drink MeadMug = new Drink("Mead (Mug)", 15, 0.05, 0.5, true);
-	public static Drink MeadGallon = new Drink("Mead (Gallon)", 16, 2, 8, true);
+	public static Drink MeadGallon = new Drink("Mead (Gallon)", 15, 2, 8, true);
+	public static Drink MeadMug = new Drink("Mead (Mug)", 16, 0.05, 0.5, true);
+	public static Drink LinnormMead = new Drink("Linnorm Mead", 17, 0.05, 0.5, true);
+	public static Drink WaspMead = new Drink("Wasp Mead", 18, 400, 2, true);
+	public static Drink Polque = new Drink("Polque", 19, 0.1, 0.5, true);
+	public static Drink Rumboozle = new Drink("Rumboozle", 20, 0.1, 0.5, true);
+	public static Drink Tepache = new Drink("Tepache", 21, 0.05, 0.5, true);
+	public static Drink DragonPunchWhiskey = new Drink("Dragon Punch Whiskey", 22, 10, true);
+	public static Drink OldlawWhiskey = new Drink("Oldlaw Whiskey", 23, 1, true);
+	public static Drink CommonWine = new Drink("Wine (Common)", 24, 0.2, 6, true);
+	public static Drink FineWhiskey = new Drink("Wine (Fine)", 25, 10, 1, true);
+	public static Drink SangwineWine = new Drink("Sangwine Wine", 26, 200, 2, true);
+	public static Drink SealordWine = new Drink("Sealord Wine", 27, 15, 0.5, true);
+	
+	#endregion
+	
+	#region Nonalcoholic Beverages
+
+	public static Drink CommonCoffee = new Drink("Coffee (Common)", 28, 0.01, 0.5, false);
+	public static Drink ExoticCoffee = new Drink("Coffee (Exotic)", 29, 0.03, 0.5, false);
+	public static Drink DesertCoffee = new Drink("Coffee (Desert)", 30, 0.02, 0.5, false);
+	public static Drink DragonsBlendCoffee = new Drink("Dragon's Blend Coffee", 31, 1, false);
+	public static Drink HybridCoffee = new Drink("Hybrid Coffee", 32, 1, false);
+	public static Drink MarchingCoffee = new Drink("Marching Coffee", 33, 1, false);
+	public static Drink Milk = new Drink("Milk", 34, 0.05, 0.5, false);
+	public static Drink PowderedMilk = new Drink("Powdered Milk", 35, 0.1, 1, false);
+	public static Drink Tea = new Drink("Tea", 36, 0.02, 0.5, false);
+	public static Drink CeremonialTea = new Drink("Ceremonial Tea", 37, 0.04, 0.5, false);
+	public static Drink MedicinalTonic = new Drink("Medicinal Tonic", 38, 10, false);
+
+	#endregion
 }
--- CGPFE/obj/Debug/net8.0/CGPFE.AssemblyInfo.cs
+++ CGPFE/obj/Debug/net8.0/CGPFE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3551ce9a2bfc04533b600b3289ef33978288b5eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68aee5827c69bdc5a5424bbacfea235616733f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
