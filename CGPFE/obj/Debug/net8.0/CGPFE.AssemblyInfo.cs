191628173590e747221edf00a1fbcbf23d6e4e62
CrystalGames2000 <ofirabe09@gmail.com>
2025-04-13 20:22:06 +0300

    Updated DLL

--- CGPFE/obj/Debug/net8.0/CGPFE.AssemblyInfo.cs
+++ CGPFE/obj/Debug/net8.0/CGPFE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68aee5827c69bdc5a5424bbacfea235616733f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a435747e343a159daae954fe203628928a799cfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
