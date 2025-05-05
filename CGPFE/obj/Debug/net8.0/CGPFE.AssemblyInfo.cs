4811adf3c173ce946ba8ad20ac5da00eeea4e2a8
User <ofirabe09@gmail.com>
2025-05-05 09:51:36 +0300

    added all skills

--- CGPFE/obj/Debug/net8.0/CGPFE.AssemblyInfo.cs
+++ CGPFE/obj/Debug/net8.0/CGPFE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b401ee8b58218ef834966b57a12deee418b55730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05b84afb669369f69c01c3569fb13e88218e453")]
 [assembly: System.Reflection.AssemblyProductAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CGPFE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
