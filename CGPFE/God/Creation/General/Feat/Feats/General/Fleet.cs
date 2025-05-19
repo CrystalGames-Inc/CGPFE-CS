083ce4cef7c0281896a2435991f0f975e2a0ad7d
User <ofirabe09@gmail.com>
2025-05-19 09:47:41 +0300

    Added all F feats

---
+++ CGPFE/God/Creation/General/Feat/Feats/General/Fleet.cs
@@ -0,0 +1,12 @@
+﻿namespace CGPFE.God.Creation.General.Feat.Feats.General;
+
+public class Fleet(): Feat("Fleet")
+{
+    public override bool CanAcquire() {
+        return true;
+    }
+
+    public override void Benefits() {
+        throw new NotImplementedException();
+    }
+}
