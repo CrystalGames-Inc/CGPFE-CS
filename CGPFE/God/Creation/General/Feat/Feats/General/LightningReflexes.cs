5822e6a2953e272cc1b175e2e8f9b94dc50e40cb
CrystalGames2000 <ofirabe09@gmail.com>
2025-05-23 23:19:41 +0300

    Added all L feats

---
+++ CGPFE/God/Creation/General/Feat/Feats/General/LightningReflexes.cs
@@ -0,0 +1,13 @@
+﻿using CGPFE.Management;
+
+namespace CGPFE.God.Creation.General.Feat.Feats.General;
+
+public class LightningReflexes(): Feat("Lightning Reflexes") {
+	public override bool CanAcquire() {
+		return true;
+	}
+
+	public override void Benefits() {
+		PlayerDataManager.Instance.Player.CombatInfo.Reflex += 2;
+	}
+}
