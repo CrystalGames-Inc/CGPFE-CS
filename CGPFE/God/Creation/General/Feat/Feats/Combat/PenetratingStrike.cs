5d431979b4eecefc1095aa33414b435fb1687a64
CrystalGames2000 <ofirabe09@gmail.com>
2025-05-24 00:08:00 +0300

    Added all P feats

---
+++ CGPFE/God/Creation/General/Feat/Feats/Combat/PenetratingStrike.cs
@@ -0,0 +1,23 @@
+﻿using CGPFE.Data.Constants;
+using CGPFE.God.Creation.General.Feat.Properties.Prerequisites;
+using CGPFE.Management;
+
+namespace CGPFE.God.Creation.General.Feat.Feats.Combat;
+
+public class PenetratingStrike: Feat {
+	public PenetratingStrike() : base("Penetrating Strike", FeatType.Combat) {
+		Prerequisites = [
+			new FeatPrerequisite("Weapon Focus"),
+			new ValuePrerequisite("Cls", 7, "=="),
+			new ValuePrerequisite("Lvl", 12)
+		];
+	}
+
+	public override bool CanAcquire() {
+		return Prerequisites.All(p => p.IsSatisfiedBy(PlayerDataManager.Instance.Player));
+	}
+
+	public override void Benefits() {
+		throw new NotImplementedException();
+	}
+}
---
+++ CGPFE/God/Creation/General/Feat/Feats/General/Persuasive.cs
@@ -0,0 +1,17 @@
+﻿using CGPFE.Management;
+
+namespace CGPFE.God.Creation.General.Feat.Feats.General;
+
+public class Persuasive(): Feat("Persuasive") {
+	public override bool CanAcquire() {
+		return true;
+	}
+
+	public override void Benefits() {
+		PlayerDataManager.Instance.Player.GetMatchingSkill("Diplomacy").Bonus.SetMiscMod(
+			PlayerDataManager.Instance.Player.GetMatchingSkill("Diplomacy").Bonus.Ranks >= 10 ? 4 : 2);
+
+		PlayerDataManager.Instance.Player.GetMatchingSkill("Intimidate").Bonus.SetMiscMod(
+			PlayerDataManager.Instance.Player.GetMatchingSkill("Intimidate").Bonus.Ranks >= 10 ? 4 : 2);
+	}
+}
