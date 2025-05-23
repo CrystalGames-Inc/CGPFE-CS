ac3460ea1656bb1a8d846218bf67f5c2481dd8cb
CrystalGames2000 <ofirabe09@gmail.com>
2025-05-24 00:19:07 +0300

    Added all R feats

---
+++ CGPFE/God/Creation/General/Feat/Feats/Combat/RapidReload.cs
@@ -0,0 +1,20 @@
+﻿using CGPFE.Data.Constants;
+using CGPFE.Management;
+
+namespace CGPFE.God.Creation.General.Feat.Feats.Combat;
+
+public class RapidReload: Feat {
+	public RapidReload() : base("Rapid Reload", FeatType.Combat) {
+		Prerequisites = [
+			//TODO add the shit
+		];
+	}
+
+	public override bool CanAcquire() {
+		return Prerequisites.All(p => p.IsSatisfiedBy(PlayerDataManager.Instance.Player));
+	}
+
+	public override void Benefits() {
+		throw new NotImplementedException();
+	}
+}
