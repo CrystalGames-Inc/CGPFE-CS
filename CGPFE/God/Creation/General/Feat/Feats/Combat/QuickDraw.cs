f6ce96c2ff3e646be9f25bc146c61a2974db32b6
CrystalGames2000 <ofirabe09@gmail.com>
2025-05-24 00:11:31 +0300

    Added all Q feats

---
+++ CGPFE/God/Creation/General/Feat/Feats/Combat/QuickDraw.cs
@@ -0,0 +1,21 @@
+﻿using CGPFE.Data.Constants;
+using CGPFE.God.Creation.General.Feat.Properties.Prerequisites;
+using CGPFE.Management;
+
+namespace CGPFE.God.Creation.General.Feat.Feats.Combat;
+
+public class QuickDraw: Feat {
+	public QuickDraw() : base("Quick Draw", FeatType.Combat) {
+		Prerequisites = [
+			new ValuePrerequisite("Bab", 1)
+		];
+	}
+
+	public override bool CanAcquire() {
+		return Prerequisites.All(p => p.IsSatisfiedBy(PlayerDataManager.Instance.Player));
+	}
+
+	public override void Benefits() {
+		throw new NotImplementedException();
+	}
+}
---
+++ CGPFE/God/Creation/General/Feat/Feats/Metamagic/QuickenSpell.cs
@@ -0,0 +1,13 @@
+﻿using CGPFE.Data.Constants;
+
+namespace CGPFE.God.Creation.General.Feat.Feats.Metamagic;
+
+public class QuickenSpell(): Feat("Quicken Spell", FeatType.Metamagic) {
+	public override bool CanAcquire() {
+		return true;
+	}
+
+	public override void Benefits() {
+		throw new NotImplementedException();
+	}
+}
